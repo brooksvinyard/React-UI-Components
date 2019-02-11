13e0904898101a6b66d866adc70e2c94cc0968ad
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-02-11 15:10:15 -0800

    Added current date

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
+let day = moment().format("DD MMM");
 
 function HeaderTitle() {
     return (
         <div className="header-title">
             <div className="title">Lambda School</div>
-            <div className="handle">@LambdaSchool - date</div>
+            <div className="handle">@LambdaSchool - {day}</div> 
         </div>
     );
 }
