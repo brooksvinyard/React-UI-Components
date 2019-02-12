998724fd41467c70aee92b3cc05e2bfb1b83f3a0
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-02-12 15:18:09 -0800

    Refactored to use buttons instead of divs. Simplified my button components.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,7 +3,86 @@ import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import NumberButton from './components/ButtonComponents/NumberButton'
 import ActionButton from './components/ButtonComponents/ActionButton'
-import ClearButton from './components/ButtonComponents/ClearButton'
+
+const One = {
+  text: '1',
+  buttonStyle: 'number one'
+};
+
+const Two = {
+  text: '2',
+  buttonStyle: 'number two'
+};
+
+const Three = {
+  text: '3',
+  buttonStyle: 'number three'
+};
+
+const Four = {
+  text: '4',
+  buttonStyle: 'number four'
+};
+
+const Five = {
+  text: '5',
+  buttonStyle: 'number five'
+};
+
+const Six = {
+  text: '6',
+  buttonStyle: 'number six'
+};
+
+const Seven = {
+  text: '7',
+  buttonStyle: 'number seven'
+};
+
+const Eight = {
+  text: '8',
+  buttonStyle: 'number eight'
+};
+
+const Nine = {
+  text: '9',
+  buttonStyle: 'number nine'
+};
+
+const Zero = {
+  text: '0',
+  buttonStyle: 'number zero'
+};
+
+const Clear = {
+  text: 'clear',
+  buttonStyle: 'clear'
+};
+
+const Divide = {
+  text: '\u00F7',
+  buttonStyle: 'action'
+};
+
+const Multiply = {
+  text: '\u00D7',
+  buttonStyle: 'action'
+};
+
+const Subtract = {
+  text: '\u2212',
+  buttonStyle: 'action'
+};
+
+const Add = {
+  text: '+',
+  buttonStyle: 'action'
+};
+
+const Equal = {
+  text: '=',
+  buttonStyle: 'action'
+};
 
 const App = () => {
   return (
@@ -14,10 +93,27 @@ const App = () => {
 
       <div className = "calc-bottom">
         <div className="bottom-left">
-          <ClearButton />
-          <NumberButton />
+          <ActionButton buttonProp={Clear}/>
+          <div className = "number-buttons">
+            <NumberButton buttonProp={One}/>
+            <NumberButton buttonProp={Two}/>
+            <NumberButton buttonProp={Three}/>
+            <NumberButton buttonProp={Four}/>
+            <NumberButton buttonProp={Five}/>
+            <NumberButton buttonProp={Six}/>
+            <NumberButton buttonProp={Seven}/>
+            <NumberButton buttonProp={Eight}/>
+            <NumberButton buttonProp={Nine}/>
+            <ActionButton buttonProp={Zero}/>
+          </div>
         </div>
-        <ActionButton />
+        <div className = "action-buttons">
+          <ActionButton buttonProp={Divide}/>
+          <ActionButton buttonProp={Multiply}/>
+          <ActionButton buttonProp={Subtract}/>
+          <ActionButton buttonProp={Add}/>
+          <ActionButton buttonProp={Equal}/>
+        </div>
       </div>
     </div>
   );
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import './Button.css';
 
-function ActionButton () {
-    return (
-        <div className = "action-buttons">
-            <div className="action divide">/</div>
-            <div className="action multiply">*</div>
-            <div className="action subtract">-</div>
-            <div className="action add">+</div>
-            <div className="action equal">=</div>
-        </div>
-    );
-}
+const ActionButton = props => {
+    return <button className={props.buttonProp.buttonStyle}>{props.buttonProp.text}</button>;
+  };
 
 export default ActionButton;
